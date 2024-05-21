0adfda511b92886305330772c7585621bf481b51
Steve·Curcy <52236967+SteveCurcy@users.noreply.github.com>
2024-05-21 10:27:22 +0800

    Update ssl.bpf.c
    patch

--- src/ssl.bpf.c
+++ src/ssl.bpf.c
@@ -331,6 +331,8 @@ int BPF_KPROBE(uprobe_pr_connect, void *fd, const struct ssl_pr_inet *addr, int 
 	__builtin_memset(&remote, 0, sizeof(remote));
 	bpf_probe_read_user(&(remote.remote_ip), sizeof(__u32), &(addr->ip));
 	bpf_probe_read_user(&(remote.remote_port), sizeof(__u16), &(addr->port));
+	remote.remote_ip = bpf_ntohl(remote.remote_ip);
+	remote.remote_port = bpf_ntohs(remote.remote_port);
 	bpf_map_update_elem(&tmp_ssl_sock, &fd, &remote, BPF_ANY);
 
 	return 0;
